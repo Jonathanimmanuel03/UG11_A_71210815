3f1b8901c4f347e13d8c09810f34e9e70297be15
jonathan <jonathanimmanuel03@gmail.com>
2021-11-26 11:59:33 +0800

    soal 2

---
+++ 2_A_71210815.py
@@ -0,0 +1,12 @@
+huruf = "abcdefg"
+
+def ambil_huruf() :
+    print("e")
+    print("f")
+    print("c")
+    
+#print(ambil_tengah("abcdefg",1))
+#print(ambil_tengah("abcdefg",2))
+#print(ambil_tengah("abcd"))
+
+ambil_huruf()
